c7382b3b8bf4e1707a5a4a37a352757cb6711b30
duopeng-czbiohub <pengxunduo@gmail.com>
2021-11-26 20:37:43 -0500

    Update DeepGenotype.py

--- DeepGenotype.py
+++ DeepGenotype.py
@@ -7,7 +7,63 @@ from subprocess import Popen
 import shutil
 import warnings
 import logging
+
 warnings.filterwarnings('ignore')
+
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+
+#The background is set with 40 plus the number of the color, and the foreground with 30
+
+#These are the sequences need to get colored ouput
+RESET_SEQ = "\033[0m"
+COLOR_SEQ = "\033[1;%dm"
+BOLD_SEQ = "\033[1m"
+
+def formatter_message(message, use_color = True):
+    if use_color:
+        message = message.replace("$RESET", RESET_SEQ).replace("$BOLD", BOLD_SEQ)
+    else:
+        message = message.replace("$RESET", "").replace("$BOLD", "")
+    return message
+
+COLORS = {
+    'WARNING': YELLOW,
+    'INFO': WHITE,
+    'DEBUG': BLUE,
+    'CRITICAL': YELLOW,
+    'ERROR': RED
+}
+
+class ColoredFormatter(logging.Formatter):
+    def __init__(self, msg, use_color = True):
+        logging.Formatter.__init__(self, msg)
+        self.use_color = use_color
+
+    def format(self, record):
+        levelname = record.levelname
+        if self.use_color and levelname in COLORS:
+            levelname_color = COLOR_SEQ % (30 + COLORS[levelname]) + levelname + RESET_SEQ
+            record.levelname = levelname_color
+        return logging.Formatter.format(self, record)
+
+# Custom logger class with multiple destinations
+class ColoredLogger(logging.Logger):
+    FORMAT = "[$BOLD%(name)-1s$RESET][%(levelname)-1s]  %(message)s " #($BOLD%(filename)s$RESET:%(lineno)d)
+    COLOR_FORMAT = formatter_message(FORMAT, True)
+    def __init__(self, name):
+        logging.Logger.__init__(self, name, logging.DEBUG)
+
+        color_formatter = ColoredFormatter(self.COLOR_FORMAT)
+
+        console = logging.StreamHandler()
+        console.setFormatter(color_formatter)
+
+        self.addHandler(console)
+        return
+
+
+logging.setLoggerClass(ColoredLogger)
+#logging.basicConfig()
 log = logging.getLogger("DeepGenotype.py")
 log.setLevel(logging.DEBUG) #set the level of warning displayed
 
@@ -92,16 +148,15 @@ def main():
 
                 log.info(f"Processing sample: {row['Sample_ID']}")
                 #run CRISPResso
-                if os.path.isfile("{path2fastqDir}/{row['Sample_ID']}{fq_ex_suffix}{fastq_R1_suffix}") and os.path.isfile("{path2fastqDir}/{row['Sample_ID']}{fq_ex_suffix}{fastq_R2_suffix}"):
+                if os.path.isfile(f"{path2fastqDir}/{row['Sample_ID']}{fq_ex_suffix}{fastq_R1_suffix}") and os.path.isfile(f"{path2fastqDir}/{row['Sample_ID']}{fq_ex_suffix}{fastq_R2_suffix}"):
                     path_to_stderr_file = os.path.join(path2_stdout, f"{row['Sample_ID']}.stderr.txt")
                     path_to_stdout_file = os.path.join(path2_stdout, f"{row['Sample_ID']}.stdout.txt")
                     mystdput = open(path_to_stdout_file, 'w+')
                     mystderr = open(path_to_stderr_file, 'w+')
                     p = Popen(command, stdout=mystdput, stderr=mystderr, universal_newlines=True)
-                    
+
                     log.info(f"...running CRISPResso")
                     p.communicate()  # wait for the commands to process
-                    log.info(f"...done")
 
                     #process allele frequency table
                     current_CRISPResso_out_dir = os.path.join(path2_CRISPResso_out,f"CRISPResso_on_{row['Sample_ID']}")
@@ -117,16 +172,14 @@ def main():
                         p = Popen(command2, universal_newlines=True)
                         log.info(f"...parsing allele frequency table and re-calculating allele frequencies")
                         p.communicate()  # wait for the commands to process
-                        log.info(f"...done")
                     else:
-                        log.warning("..cannot find CRISPResso output file: Alleles_frequency_table.zip ")
+                        log.warning("...cannot find CRISPResso output file: Alleles_frequency_table.zip ")
                     if os.path.isfile(os.path.join(current_CRISPResso_out_dir, "genotype_frequency.csv")):
-                        log.info("Writing/appending alleles frequencies to: genotype_frequency.csv (in the working directory)")
                         with open(os.path.join(current_CRISPResso_out_dir, "genotype_frequency.csv"), "r") as handle:
                             next(handle)
                             writehandle.write(f"{row['Sample_ID']},")
                             writehandle.write(handle.readline())
-                        log.info("...done")
+                    log.info("...done")
                 else:
                     log.warning(f"...cannot find the fastq files, please check the filenames and the paths")
 
